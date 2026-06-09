c161028ecc6e6cc3da5b1ca631783dfbfde74a67
Heiligetomate <heiligetomate@protonmail.com>
2026-06-09 23:02:16 +0200

    fields cant be empty

--- puwump/src/ui/add_exercise.rs
+++ puwump/src/ui/add_exercise.rs
@@ -12,6 +12,24 @@ pub struct AddExerciseForm {
     pub status: Option<Result<(), String>>,
 }
 
+impl AddExerciseForm {
+    fn reset(&mut self) {
+        self.name = String::new();
+        self.instructions = String::new();
+    }
+
+    fn is_empty(&self) -> bool {
+        if self.name.is_empty() || self.instructions.is_empty() {
+            return true;
+        }
+        false
+    }
+
+    fn set_err(&mut self, message: &str) {
+        self.status = Some(Err(message.to_owned()));
+    }
+}
+
 impl PuwumpUi {
     pub fn add_exercise_view(&mut self, ui: &mut Ui) {
         let width = ui.available_width();
@@ -54,15 +72,23 @@ impl PuwumpUi {
             ui.add_space(height * 0.02);
 
             if self.button(ui, inner_width * 0.5, height * 0.07, Color32::from_rgb(184, 187, 38), "Confirm") {
-                match self
-                    .db
-                    .new_exercise(&self.add_exercise.name, &self.add_exercise.instructions)
-                {
-                    Ok(_) => self.add_exercise.status = Some(Ok(())),
-                    Err(e) => match e {
-                        PuwumpError::UniqueViolation => self.add_exercise.status = Some(Err(e.to_string())),
-                        _ => panic!("db is broken"),
-                    },
+                if self.add_exercise.is_empty() {
+                    self.add_exercise
+                        .set_err("Fill out both fields");
+                } else {
+                    match self
+                        .db
+                        .new_exercise(&self.add_exercise.name, &self.add_exercise.instructions)
+                    {
+                        Ok(_) => {
+                            self.add_exercise.status = Some(Ok(()));
+                            self.add_exercise.reset();
+                        }
+                        Err(e) => match e {
+                            PuwumpError::UniqueViolation => self.add_exercise.status = Some(Err(e.to_string())),
+                            _ => panic!("db is broken"),
+                        },
+                    }
                 }
             }
 
