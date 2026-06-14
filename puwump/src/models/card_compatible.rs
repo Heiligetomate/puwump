2ae9636c94e5753deb3401644121b66354dd72d2
Heiligetomate <heiligetomate@protonmail.com>
2026-06-15 00:17:37 +0200

    add todo

--- puwump/src/models/card_compatible.rs
+++ puwump/src/models/card_compatible.rs
@@ -2,6 +2,13 @@ use uuid::Uuid;
 
 use crate::{db::Db, errors::Result, models::core::Model};
 
+// TODO: CardAdd should not handle the input fields
+// Add a new trait just for the input fields
+// The new trait should have a vec with the required info
+// It also stores the input data for each input field
+// The trait shold then be stored either in the TaskHandler with a new generic parameter
+// Or the CardAdd could hold a type to the input field trait
+
 pub trait CardAdd: Model {
     fn title(&self) -> &str;
     fn body(&self) -> Option<&str>;
